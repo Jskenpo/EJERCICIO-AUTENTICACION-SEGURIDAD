e56c68326bed6a64eb83881f09ab0c6fdd24bf51
Mendezg1 <87867522+Mendezg1@users.noreply.github.com>
2025-02-06 23:37:51 -0600

    Final swap to jwt

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM node:18
 
 COPY package*.json ./
 RUN npm install
-RUN npm install express-session
-RUN npm install keycloak-connect
 
 COPY . .
 
--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 const express = require('express');
-const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const jwkToPem = require('jwk-to-pem');
+
 const app = express();
-const router = require('./routes/index');
-const {initOIDC} = require('./keycloak/keycloak');
+app.use(bodyParser.json());
 
+// Define la clave pública directamente como una constante
+const publicKeyJwk = {
+  kty: 'RSA',  // Tipo de clave (RSA)
+  n: 'qISnG_q2pzoCLQH8Zx1O99UL0taee_XiL6e0kk5ahIb2Hey1kKzI2xsN7GPJ-Uzu7DIR3StIsZYCH94pI_AmpExQrNbHXsKlWrTp8NhgrOZjMTE-6EM6h0mXH7tMeiB1gMMO0IvX4KoUfLgQ1knyOmyL8KOtcsMasBoECPxziUtr4its7J-ACBwnL5wDTmsGXbVxJYQ1W98_WFtvCYfm88o5SJAnarUJaY8elPAx8aYzuiHPYbJiMMko5_ACAQtQm4FB0j9WgDLquJ1XWH5mL81dddAnRvCzkQTtS_ZI3DPLu2l5DcWNxV-piETyVegSToEXLEx5xT00RzGOoUhf0w', // Módulo
+  e: 'AQAB'  // Exponente público (comúnmente es 'AQAB' que es 65537 en decimal)
+};
 
-//middlewares
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+// Convierte la clave JWK a PEM
+const publicKeyPem = jwkToPem(publicKeyJwk);
 
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1];
 
-// Enable CORS
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+  if (!token) {
+    return res.status(401).json({ message: 'Token requerido' });
+  }
 
-
-initOIDC().then(() => {
-    app.use('/api', router);
-  
-    app.listen(3000, () => {
-      console.log('Servidor corriendo en http://localhost:3000');
+  try {
+    jwt.verify(token, publicKeyPem, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Token inválido o expirado' });
+      }
+      req.user = decoded;
+      next();
     });
-  }).catch(err => {
-    console.error('Error al inicializar OIDC:', err);
-  });
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al verificar el token' });
+  }
+};
+
+app.get('/public', (req, res) => {
+  res.json({ message: 'Esta ruta es pública' });
+});
+
+app.get('/private', verifyToken, (req, res) => {
+  res.json({ message: 'Acceso autorizado con token válido', user: req.user });
+});
+
+app.post('/data', verifyToken, (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.json({ message: 'Datos recibidos', receivedData: data });
+});
+
+app.listen(3000, () => {
+  console.log('API REST escuchando en http://localhost:3000');
+});
